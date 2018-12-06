9a704149ee8b675e0269b9b2fa132ee49b197b3b
Carpoor <836668616@qq.com>
2018-12-06 22:44:00 +0800

    新增eureka服务注册中心

---
+++ microservicecloud-eureka-7001/src/main/java/com/cwq/springcloud/EurekaServer7001_App.java
@@ -0,0 +1,19 @@
+package com.cwq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+/**
+ * 
+ * @author Carpoor  
+ * @date 2018年12月6日
+ */
+@SpringBootApplication
+@EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
+public class EurekaServer7001_App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer7001_App.class, args);
+	}
+
+}
