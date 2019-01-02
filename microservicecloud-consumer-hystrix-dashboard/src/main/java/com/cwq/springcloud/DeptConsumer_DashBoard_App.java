a1ee0ccd0dc5a402690a1a362bbfd154be8cc410
Carpoor <836668616@qq.com>
2019-01-02 23:18:02 +0800

    hystrix监控平台备注

--- microservicecloud-consumer-hystrix-dashboard/src/main/java/com/cwq/springcloud/DeptConsumer_DashBoard_App.java
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/cwq/springcloud/DeptConsumer_DashBoard_App.java
@@ -5,11 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /** 
+ * 被监视的hystrix模块，需要加入actuator
+ * http://localhost:9001/hystrix  web平台配置需要监控的模块
+ * 				^^
+ * 				配置
+ * 				||
+ * http://localhost:8001/hystrix.stream 一直刷新服务的调用信息，不过都是json串，监控平台将这些json数据进行图形化展示 
  * @author Carpoor  
  * @date 2019年1月2日 
  */
 @SpringBootApplication
-@EnableHystrixDashboard
+@EnableHystrixDashboard//开启监视器
 public class DeptConsumer_DashBoard_App {
 
 	public static void main(String[] args) {
